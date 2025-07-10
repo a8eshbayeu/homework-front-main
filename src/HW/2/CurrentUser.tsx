28f21370d83cc20324f98e10e70bc42e56694588
Artur Eshbayeu <a.eshbayeu@gmail.com>
2025-07-10 20:13:52 +0200

    first commit

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,7 +1,7 @@
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: UserType // пропиши типизацию
+  user: UserType  // пропиши типизацию
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
